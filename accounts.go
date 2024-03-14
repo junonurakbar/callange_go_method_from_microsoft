228444b263791756d3dd06c8151bba71a7ed233b
junonurakbar <junonurakbar@gmail.com>
2024-03-14 17:12:33 +0700

    initialize go module

---
+++ accounts.go
@@ -0,0 +1,28 @@
+package challange
+
+type Account struct {
+	FirstName string
+	LastName  string
+}
+
+func (a *Account) ChangeName(first, last string) {
+	a.FirstName = first
+	a.LastName = last
+}
+
+type Employee struct {
+	Account
+	credits float64
+}
+
+func (e Employee) AddCredits(balance float64) float64 {
+	e.credits += balance
+	return e.credits
+}
+func (e Employee) RemoveCredits(balance float64) float64 {
+	e.credits -= balance
+	return e.credits
+}
+func (e Employee) GetCredits() float64 {
+	return e.credits
+}
